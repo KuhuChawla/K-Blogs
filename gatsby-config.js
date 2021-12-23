d39b6ccfd79a68312d29316fe69765eff368e7ef
KuhuChawla <72125043+KuhuChawla@users.noreply.github.com>
2021-12-23 18:29:34 +0530

    final3.0

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
       summary: `Pre-Final year student pursuing Bachelors of Engineering in Electronics and Communication from UIET, Panjab University,Chandigarh.`,
     },
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+      'Kuhu Chawla is a pre-final year student pursuing Bachelors of Engineering in Electronics and Communication from UIET, Panjab University,Chandigarh.',
     siteUrl: 'https://kuhu.tech', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@kuhuchawla01',
